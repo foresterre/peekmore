2c3597bfa2027abf79b694c2c6a7d1700b1b74b0
foresterre <garm@ilumeo.com>
2019-09-09 20:54:36 +0200

    Update naming to peekmore.

---
+++ README.md
@@ -0,0 +1,7 @@
+# peekmore
+
+Iterator which allows one to peek further than one element.
+
+See [documentation](https://docs.rs/peekmore/).
+
+Experimental.
--- src/lib.rs
+++ src/lib.rs
@@ -5,14 +5,14 @@ extern crate alloc;
 use alloc::vec::Vec;
 
 /// Trait which allows one to create an iterator which allows us to peek multiple times forward.
-pub trait CreateLookAheadIterator: Iterator + Sized {
+pub trait CreatePeekMoreIterator: Iterator + Sized {
     /// Create an iterator where we can look (peek) forward multiple times from an existing iterator.
-    fn look_ahead(self) -> LookAheadIterator<Self>;
+    fn peekmore(self) -> PeekMoreIterator<Self>;
 }
 
-impl<I: Iterator> CreateLookAheadIterator for I {
-    fn look_ahead(self) -> LookAheadIterator<I> {
-        LookAheadIterator {
+impl<I: Iterator> CreatePeekMoreIterator for I {
+    fn peekmore(self) -> PeekMoreIterator<I> {
+        PeekMoreIterator {
             iterator: self,
             queue: Vec::new(),
             needle: None,
@@ -24,7 +24,7 @@ impl<I: Iterator> CreateLookAheadIterator for I {
 /// In reality the underlying iterator will be consumed, but the values will be stored in a local
 /// vector. This vector allows us to move the frame which we can see.
 #[derive(Debug)]
-pub struct LookAheadIterator<I: Iterator> {
+pub struct PeekMoreIterator<I: Iterator> {
     /// Inner iterator. Consumption of this iterator does not represent consumption of the LookAheadIterator.
     iterator: I,
 
@@ -39,16 +39,18 @@ pub struct LookAheadIterator<I: Iterator> {
 }
 
 /// Adds functions which enable non-consuming viewing of non-consumed elements of an iterator.
-pub trait LookAheadView<I: Iterator> {
+pub trait PeekView<I: Iterator> {
     // methods to preview the next non consumed elements of the iterator
 
     /// Get the reference of our current value preview.
     /// Note that if we have not advanced our position yet, our view will return None, even if
     /// the underlying iterator does contain values, and thus is not empty.
-    fn preview(&mut self) -> Option<&I::Item>;
+    ///
+    /// TODO: perhaps do peek at the first item always, it does make more sense?
+    fn peek(&mut self) -> Option<&I::Item>;
 
     /// Advance the view to the next frame and return a reference to its value.
-    fn preview_next(&mut self) -> Option<&I::Item>;
+    fn peek_next(&mut self) -> Option<&I::Item>;
 
     // methods to control our view
 
@@ -57,14 +59,14 @@ pub trait LookAheadView<I: Iterator> {
     /// To advance the iterator, use `Iterator::next()`.
     fn advance_view(&mut self) -> &mut Self;
 
-    /// Reset the view. If we call [crate::multi_peek::LookAheadView::preview] just after a reset,
+    /// Reset the view. If we call [crate::PeekView::preview] just after a reset,
     /// it will return None again. The view will have to be advanced once for a non empty iterator to
     /// show a value again.
     fn reset_view(&mut self);
 }
 
-impl<I: Iterator> LookAheadView<I> for LookAheadIterator<I> {
-    fn preview(&mut self) -> Option<&I::Item> {
+impl<I: Iterator> PeekView<I> for PeekMoreIterator<I> {
+    fn peek(&mut self) -> Option<&I::Item> {
         if let Some(needle) = self.needle {
             self.queue.get(needle).and_then(|v| v.as_ref())
         } else {
@@ -73,12 +75,12 @@ impl<I: Iterator> LookAheadView<I> for LookAheadIterator<I> {
     }
 
     // convenient as we don't have to re-assign our mutable borrow on the 'user' side.
-    fn preview_next(&mut self) -> Option<&I::Item> {
+    fn peek_next(&mut self) -> Option<&I::Item> {
         let this = self.advance_view();
-        this.preview()
+        this.peek()
     }
 
-    fn advance_view(&mut self) -> &mut LookAheadIterator<I> {
+    fn advance_view(&mut self) -> &mut PeekMoreIterator<I> {
         match self.needle {
             None if self.queue.is_empty() => {
                 self.push_next_to_queue();
@@ -97,7 +99,7 @@ impl<I: Iterator> LookAheadView<I> for LookAheadIterator<I> {
     }
 }
 
-impl<I: Iterator> LookAheadIterator<I> {
+impl<I: Iterator> PeekMoreIterator<I> {
     /// Consume the underlying iterator and push an element to the queue.
     fn push_next_to_queue(&mut self) {
         let item = self.iterator.next();
@@ -126,7 +128,7 @@ impl<I: Iterator> LookAheadIterator<I> {
     }
 }
 
-impl<'a, I: Iterator> Iterator for LookAheadIterator<I> {
+impl<'a, I: Iterator> Iterator for PeekMoreIterator<I> {
     type Item = I::Item;
 
     fn next(&mut self) -> Option<Self::Item> {
@@ -150,47 +152,47 @@ mod tests {
     fn peek_forward_with_reassignment() {
         let iterable = [1, 2, 3, 4];
 
-        let mut peek = iterable.iter().look_ahead();
+        let mut peek = iterable.iter().peekmore();
 
-        assert_eq!(peek.preview(), None);
-
-        let peek = peek.advance_view();
-        assert_eq!(peek.preview(), Some(&&1));
+        assert_eq!(peek.peek(), None);
 
         let peek = peek.advance_view();
-        assert_eq!(peek.preview(), Some(&&2));
+        assert_eq!(peek.peek(), Some(&&1));
 
         let peek = peek.advance_view();
-        assert_eq!(peek.preview(), Some(&&3));
+        assert_eq!(peek.peek(), Some(&&2));
 
         let peek = peek.advance_view();
-        assert_eq!(peek.preview(), Some(&&4));
+        assert_eq!(peek.peek(), Some(&&3));
 
         let peek = peek.advance_view();
-        assert_eq!(peek.preview(), None);
+        assert_eq!(peek.peek(), Some(&&4));
+
+        let peek = peek.advance_view();
+        assert_eq!(peek.peek(), None);
     }
 
     #[test]
     fn peek_forward_without_reassignment_separately_advance_and_peek() {
         let iterable = [1, 2, 3, 4];
 
-        let mut iter = iterable.iter().look_ahead();
+        let mut iter = iterable.iter().peekmore();
 
-        assert_eq!(iter.preview(), None);
+        assert_eq!(iter.peek(), None);
 
-        let v1 = iter.advance_view().preview();
+        let v1 = iter.advance_view().peek();
         assert_eq!(v1, Some(&&1));
 
-        let v2 = iter.advance_view().preview();
+        let v2 = iter.advance_view().peek();
         assert_eq!(v2, Some(&&2));
 
-        let v3 = iter.advance_view().preview();
+        let v3 = iter.advance_view().peek();
         assert_eq!(v3, Some(&&3));
 
-        let v4 = iter.advance_view().preview();
+        let v4 = iter.advance_view().peek();
         assert_eq!(v4, Some(&&4));
 
-        let v5 = iter.advance_view().preview();
+        let v5 = iter.advance_view().peek();
         assert_eq!(v5, None);
     }
 
@@ -198,23 +200,23 @@ mod tests {
     fn peek_forward_without_reassignment_advance_and_peek_combined() {
         let iterable = [1, 2, 3, 4];
 
-        let mut iter = iterable.iter().look_ahead();
+        let mut iter = iterable.iter().peekmore();
 
-        assert_eq!(iter.preview(), None);
+        assert_eq!(iter.peek(), None);
 
-        let v1 = iter.preview_next();
+        let v1 = iter.peek_next();
         assert_eq!(v1, Some(&&1));
 
-        let v2 = iter.preview_next();
+        let v2 = iter.peek_next();
         assert_eq!(v2, Some(&&2));
 
-        let v3 = iter.preview_next();
+        let v3 = iter.peek_next();
         assert_eq!(v3, Some(&&3));
 
-        let v4 = iter.preview_next();
+        let v4 = iter.peek_next();
         assert_eq!(v4, Some(&&4));
 
-        let v5 = iter.preview_next();
+        let v5 = iter.peek_next();
         assert_eq!(v5, None);
     }
 
@@ -222,32 +224,32 @@ mod tests {
     fn peek_forward_without_reassignment_advance_and_peek_combined_and_reset_view() {
         let iterable = [1, 2, 3, 4];
 
-        let mut iter = iterable.iter().look_ahead();
+        let mut iter = iterable.iter().peekmore();
 
-        assert_eq!(iter.preview(), None);
+        assert_eq!(iter.peek(), None);
 
-        let v1 = iter.preview_next();
+        let v1 = iter.peek_next();
         assert_eq!(v1, Some(&&1));
 
-        let v2 = iter.preview_next();
+        let v2 = iter.peek_next();
         assert_eq!(v2, Some(&&2));
 
         let _ = iter.reset_view();
-        assert_eq!(iter.preview(), None);
+        assert_eq!(iter.peek(), None);
 
-        let v1again = iter.preview_next();
+        let v1again = iter.peek_next();
         assert_eq!(v1again, Some(&&1));
 
-        let v2again = iter.preview_next();
+        let v2again = iter.peek_next();
         assert_eq!(v2again, Some(&&2));
 
-        let v3 = iter.preview_next();
+        let v3 = iter.peek_next();
         assert_eq!(v3, Some(&&3));
 
-        let v4 = iter.preview_next();
+        let v4 = iter.peek_next();
         assert_eq!(v4, Some(&&4));
 
-        let v5 = iter.preview_next();
+        let v5 = iter.peek_next();
         assert_eq!(v5, None);
     }
 
@@ -255,76 +257,76 @@ mod tests {
     fn empty() {
         let iterable: [i32; 0] = [];
 
-        let mut iter = iterable.iter().look_ahead();
+        let mut iter = iterable.iter().peekmore();
 
-        assert_eq!(iter.preview(), None);
+        assert_eq!(iter.peek(), None);
 
-        let none = iter.preview_next();
+        let none = iter.peek_next();
         assert_eq!(none, None);
 
         let iter = iter.advance_view();
-        assert_eq!(iter.preview(), None);
-        assert_eq!(iter.preview_next(), None);
+        assert_eq!(iter.peek(), None);
+        assert_eq!(iter.peek_next(), None);
     }
 
     #[test]
     fn test_with_consume() {
         let iterable = "123".chars();
 
-        let mut iter = iterable.look_ahead();
+        let mut iter = iterable.peekmore();
         assert_eq!(
-            iter.preview_next(),
+            iter.peek_next(),
             Some(&core::char::from_digit(1, 10).unwrap())
         );
         assert_eq!(
-            iter.preview_next(),
+            iter.peek_next(),
             Some(&core::char::from_digit(2, 10).unwrap())
         );
         assert_eq!(
-            iter.preview_next(),
+            iter.peek_next(),
             Some(&core::char::from_digit(3, 10).unwrap())
         );
-        assert_eq!(iter.preview_next(), None);
+        assert_eq!(iter.peek_next(), None);
         assert_eq!(iter.next(), Some(core::char::from_digit(1, 10).unwrap()));
         assert_eq!(iter.next(), Some(core::char::from_digit(2, 10).unwrap()));
         assert_eq!(iter.next(), Some(core::char::from_digit(3, 10).unwrap()));
         assert_eq!(iter.next(), None);
-        assert_eq!(iter.preview_next(), None);
+        assert_eq!(iter.peek_next(), None);
     }
 
     #[test]
     fn test_with_consume_and_reset() {
         let iterable = "456".chars();
 
-        let mut iter = iterable.look_ahead();
+        let mut iter = iterable.peekmore();
         assert_eq!(
-            iter.preview_next(),
+            iter.peek_next(),
             Some(&core::char::from_digit(4, 10).unwrap())
         );
         assert_eq!(
-            iter.preview_next(),
+            iter.peek_next(),
             Some(&core::char::from_digit(5, 10).unwrap())
         );
         assert_eq!(
-            iter.preview_next(),
+            iter.peek_next(),
             Some(&core::char::from_digit(6, 10).unwrap())
         );
-        assert_eq!(iter.preview_next(), None);
+        assert_eq!(iter.peek_next(), None);
         assert_eq!(iter.next(), Some(core::char::from_digit(4, 10).unwrap()));
         iter.reset_view();
 
         assert_eq!(
-            iter.preview_next(),
+            iter.peek_next(),
             Some(&core::char::from_digit(5, 10).unwrap())
         );
         assert_eq!(
-            iter.preview_next(),
+            iter.peek_next(),
             Some(&core::char::from_digit(6, 10).unwrap())
         );
 
         assert_eq!(iter.next(), Some(core::char::from_digit(5, 10).unwrap()));
         assert_eq!(iter.next(), Some(core::char::from_digit(6, 10).unwrap()));
         assert_eq!(iter.next(), None);
-        assert_eq!(iter.preview_next(), None);
+        assert_eq!(iter.peek_next(), None);
     }
 }
